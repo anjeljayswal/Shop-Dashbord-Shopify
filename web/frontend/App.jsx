6158f520b26b9586189b10f88c404a09e73d8ca0
anjeljayswal <anjeljayswal@gmail.com>
2025-06-26 14:57:40 +0530

    navbar created

---
+++ web/frontend/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { NavMenu } from "@shopify/app-bridge-react";
+import Routes from "./Routes";
+
+import { QueryProvider, PolarisProvider,NavigationBar } from "./components";
+// import NavigationBar from "./components/NavigationBar";
+
+export default function App() {
+  // Any .tsx or .jsx files in /pages will become a route
+  // See documentation for <Routes /> for more info
+const pages = import.meta.glob('./pages/**/*.[jt]sx', {
+  eager: true,
+  // ignore: ['**/*.test.*']
+});  const { t } = useTranslation();
+
+  return (
+    <PolarisProvider>
+      <BrowserRouter>
+        <QueryProvider>
+          <NavMenu>
+            <a href="/" rel="home" />
+            <a href="/pagename">{t("NavigationMenu.pageName")}</a>
+          </NavMenu>
+          <div className="main-section">
+            <div className="menu-section">
+                <NavigationBar/>
+            </div>
+            <div className="content-section">
+              <Routes pages={pages} />
+            </div>
+          </div>
+        </QueryProvider>
+      </BrowserRouter>
+    </PolarisProvider>
+  );
+}
---
+++ web/frontend/components/index.js
@@ -0,0 +1,3 @@
+export { ProductsCard } from "./ProductsCard";
+export { NavigationBar } from "./NavigationBar";
+export * from "./providers";
