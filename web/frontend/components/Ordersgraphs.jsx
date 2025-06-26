5dccc70fab767dff320a29d0a8a86085d29268aa
anjeljayswal <anjeljayswal@gmail.com>
2025-06-26 16:06:46 +0530

    add chart.js and react-chartjs-2 dependencies; implement Ordersgraphs component with line, doughnut, and bar charts

--- web/frontend/components/Ordersgraphs.jsx
+++ web/frontend/components/Ordersgraphs.jsx
@@ -1,23 +1,37 @@
 import { Layout, LegacyCard } from '@shopify/polaris'
-import React from 'react'
+import React, { useState } from 'react'
+import {storeData} from "../data";
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Line,Bar, Doughnut } from 'react-chartjs-2';
 
 export function Ordersgraphs() {
+    console.log(storeData);
+    let [data, setData] = useState({
+        labels: storeData.map((item) => item.year),
+        datasets: [{
+            label:"Order Details",
+            data: storeData.map((item) => item.order),
+            backgroundColor: ['#008170','#000000','#8e8e8e','#81BF37'],
+        }]
+    });
     return (
         <>
             <Layout>
                 <Layout.Section oneHalf>
                     <LegacyCard title="Total Orders" sectioned>
-
+                        <Line data={data} options={{responsive:true, maintainAspectRatio:false}}  />
+                        {/* <Pie data={data} /> */}
                     </LegacyCard>
                 </Layout.Section>
                 <Layout.Section oneThird>
                     <LegacyCard title="Completed Orders" sectioned>
+                        <Doughnut data={data} options={{responsive:true, maintainAspectRatio:false}} />
 
                     </LegacyCard>
                 </Layout.Section>
                 <Layout.Section oneThird>
                     <LegacyCard title="Remaining Orders" sectioned>
-
+                        <Bar data={data} options={{responsive:true, maintainAspectRatio:false}} />
                     </LegacyCard>
                 </Layout.Section>
             </Layout>
